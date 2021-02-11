1e268d8236248a1056234ef7d03aeffbc18df915
girisha <girisha.devara@gmail.com>
2021-02-11 16:15:46 +0530

    day 2 afternoon files added

--- giet/myapp2/urls.py
+++ giet/myapp2/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from myapp2 import views
 urlpatterns = [
 	path('homeurl/',views.home,name="home"),
+	path('index/',views.index , name= 'index'),
+	path('student',views.student, name = 'student'),
+	path('<int:val>',views.value,name= 'value'),#localhost:myapp2/67
+	path("table/<int:v>",views.table, name= 'table'),
 ]
--- giet/myapp2/views.py
+++ giet/myapp2/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 # Create your views here.
 def stat(request):
 	return HttpResponse("Iam from Myapp2")
 def home(request):
 	return HttpResponse("Iam from Myapp2 Urls")
+
+def index(request):
+	value = random.randint(1,100)
+	return render(request, 'index.html',{'name':value})
+
+def student(request):
+	details = {'name':'Gopi','number':44, 'branch':'cse'}
+	return render(request,"student_info.html",{'details':details})
+
+def value(request, val):
+	a = []
+	for i in range(1,val+1):
+		a.append(i)
+	return render(request,"value.html",{'value':a})
+
+def table(request, v):
+	a = []
+	for i in range(1,11):
+		a.append(i*v)
+	return render(request,'table.html',{'array':a,'v':v})
