4d22a9910653c58a9378bbc7fd75be28726cecb8
pavani <durga.k@apssdc.in>
2021-02-15 09:47:46 +0530

    models

--- giet/myapp2/models.py
+++ giet/myapp2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	Gender_choices=[('Female','Female'),('Male','Male')]
+	FullName=models.CharField(max_length=30)
+	RollNo=models.CharField(max_length=10)
+	EmailId=models.EmailField()
+	MobileNo=models.CharField(max_length=10)
+	Gender=models.CharField(max_length=6,choices=Gender_choices)
+	Address=models.TextField()
